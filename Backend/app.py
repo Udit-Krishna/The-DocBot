b1775095d375418b9d5a1c7ee2fe57f79d3b8234
Udit Krisha <uditkrishna2003@gmail.com>
2024-08-04 21:04:00 +0530

    increased temperature in chatbot for better answers

--- Backend/app.py
+++ Backend/app.py
@@ -79,7 +79,7 @@ llm = HuggingFaceEndpoint(
     task="text-generation",
     max_new_tokens=180,
     do_sample=False,
-    temperature=0.1,
+    temperature=0.5,
     token=api_key
 )
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
